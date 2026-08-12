48169af7b6e432b354fd3ed73282651399ba18ef
Yewen Jin <yewen.elsa.jin@gmail.com>
2026-08-12 16:29:54 +0100

    change connection type

--- src/__tests__/db-sanity.test.ts
+++ src/__tests__/db-sanity.test.ts
@@ -5,7 +5,6 @@ afterAll(async () => {
 });
 
 test("can connect to the database", async () => {
-  console.log("DATABASE_URL:", process.env.DATABASE_URL);
   const res = await pool.query("SELECT NOW()");
 
   expect(res.rows.length).toBe(1);
